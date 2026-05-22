14c82771259c366705ba50075645f060b3d3ad8c
nijika-boyfriend <266692516+nijika-boyfriend@users.noreply.github.com>
2026-05-22 10:22:50 +0800

    bump: 升级版本至 1.1.1(3) 并更新 LSPosed 模块描述

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "yuki.biliblocker"
         minSdk = 21
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     signingConfigs {
